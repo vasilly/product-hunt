2f123d0daebdaad9fab57738494d9c87cf05b2c4
vasilly <vasko.traikovski@gmail.com>
2016-07-03 20:48:25 +1000

    initial flux src-actions-index.js file correct

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,26 @@
-import Alt from '../alt';
+import alt from '../alt';
 import Firebase from 'firebase';
 
 class Actions {
 
-  login ()  {
+  login() {
     return (dispatch) => {
-    var firebaseRef = new Firebase('https://product-hunt.firebaseio.com');
-    firebaseRef.authWithOAuthPopup('facebook', (error, authData) => {
-      if(error){
-        return;
-      }
-      var user = {
-        id:authData.facebook.id,
-        name:authData.facebook.isplayName,
-        avatar:authData.facebook.profileImageURL,
-      }
-      firebaseRef.child("users").child(authData.facebook.id).set(user)
-      dispatch(user);
+      var firebaseRef = new Firebase('https://codehunt-demo.firebaseio.com');
+      firebaseRef.authWithOAuthPopup('facebook', (error, authData) => {
+        if (error) {
+          return;
+        }
+
+        var user = {
+          id: authData.facebook.id,
+          name: authData.facebook.displayName,
+          avatar: authData.facebook.profileImageURL
+        }
+        firebaseRef.child("users").child(authData.facebook.id).set(user);
+        dispatch(user);
       });
     }
   }
 }
 
 export default alt.createActions(Actions);
-
-
-
-
